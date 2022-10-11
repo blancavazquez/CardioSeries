f5ad7d41a925f15f80dc2a5b84042b13b02b37f3
Blanca Vázquez <35945087+blancavazquez@users.noreply.github.com>
2022-10-11 14:09:56 -0500

    Add files via upload

---
+++ forecasting/loss/mmd_loss.py
@@ -0,0 +1,41 @@
+#source https://github.com/ratschlab/RGAN
+
+import torch
+from . import soft_dtw
+from . import path_soft_dtw 
+
+def mmd_loss(outputs, targets, device,kernel="rbf"):
+    """Emprical maximum mean discrepancy. The lower the result, the more evidence that distributions are the same.
+
+    Args:
+        x: first sample, distribution P
+        y: second sample, distribution Q
+        kernel: kernel type such as "multiscale" or "rbf"
+    """
+
+    outputs = outputs.reshape(outputs.shape[0],outputs.shape[1]*outputs.shape[2])
+    targets = targets.reshape(targets.shape[0],targets.shape[1]*targets.shape[2])
+
+    xx, yy, zz = torch.mm(outputs, outputs.t()), torch.mm(targets, targets.t()), torch.mm(outputs, targets.t())
+    rx = (xx.diag().unsqueeze(0).expand_as(xx))
+    ry = (yy.diag().unsqueeze(0).expand_as(yy))
+    
+    dxx = rx.t() + rx - 2. * xx # Used for A in (1)
+    dyy = ry.t() + ry - 2. * yy # Used for B in (1)
+    dxy = rx.t() + ry - 2. * zz # Used for C in (1)
+    
+    XX, YY, XY = (torch.zeros(xx.shape).to(device),
+                  torch.zeros(xx.shape).to(device),
+                  torch.zeros(xx.shape).to(device))
+                
+    if kernel == "rbf":
+      
+        bandwidth_range = [10, 15, 20, 50]
+        for a in bandwidth_range:
+            XX += torch.exp(-0.5*dxx/a)
+            YY += torch.exp(-0.5*dyy/a)
+            XY += torch.exp(-0.5*dxy/a)
+      
+      
+
+    return torch.mean(XX + YY - 2. * XY)
