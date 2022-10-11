e479bc456b5619ce12075cc858ada3d94741dc8b
Blanca Vázquez <35945087+blancavazquez@users.noreply.github.com>
2022-10-11 16:01:45 -0500

    Add files via upload

---
+++ forecasting/training.sh
@@ -0,0 +1,23 @@
+# Author: Blanca Vázquez
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "Pipeline for time series forecasting (training)"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+python forecasting/training.py --foldername "dilate_" \
+       --epochs "1000" \
+       --batch_size "32" \
+       --learning_rate "0.01" \
+       --decay "1e-8" \
+       --net "CausalModel" \
+       --alpha "0.3" \
+       --gamma "0.01"  \
+       --kfold "5"  \
+       --input_seq_length "6" \
+       --output_seq_length "6" \
+       --output_rnn "100" \
+       --num_features "7" \
+       --conditions "5" \
+       --missing_values "True" \
+       --saits_impute "True" \
+       --feature_list "arterial_bp_mean respiratory_rate diastolic_bp spo2 heart_rate systolic_bp temperature"
